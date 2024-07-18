01ead208ee39f8823549ff74d7799de059be1f61
Hudson Kass <kass.h@northeastern.edu>
2024-07-19 01:23:42 +0300

    added model

--- src/sentiment-analysis.py
+++ src/sentiment-analysis.py
@@ -1,9 +1,14 @@
 from transformers import AutoModelForSequenceClassification, Trainer, TrainingArguments
-from datasets import load_dataset
+from datasets import Dataset
 import pandas as pd
 
 #First we want to read in the training data
-data_path = 'path_to_data_file'
+data_path = 'data/data.csv'
 data = pd.read_csv(data_path)
 
+# Convert the DataFrame into a Hugging Face dataset
+dataset = Dataset.from_pandas(data)
+
+#Bring in the model
+model_name = "finiteautomata/bertweet-base-sentiment-analysis"
    
