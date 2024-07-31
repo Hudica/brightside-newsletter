3fa7887204e7fb7d58c399b12effe693410d80b2
Hudson Kass <kass.h@northeastern.edu>
2024-07-31 02:14:48 +0200

    new improved model

--- src/sentiment-analysis.py
+++ src/sentiment-analysis.py
@@ -1,24 +1,22 @@
 from transformers import AutoModelForSequenceClassification, Trainer, TrainingArguments, AutoTokenizer
 from datasets import Dataset
-
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
-#First we want to read in the training data
+# Read the training data
 data_path = 'data/data.csv'
 data = pd.read_csv(data_path)
 
 # Convert the DataFrame into a Hugging Face dataset
 dataset = Dataset.from_pandas(data)
 
-#Bring in the model
-model_name = "finiteautomata/bertweet-base-sentiment-analysis"
-
+# Load the tokenizer and model
+model_name = "cardiffnlp/twitter-roberta-base-sentiment-latest"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=3)
 
 # Function to tokenize the data
 def tokenize_function(examples):
-    return tokenizer(examples['headline'], truncation=True, padding="max_length", max_length=128)
+    return tokenizer(examples['headline'], truncation=True, padding="max_length", max_length=64)
 
 # Apply tokenization to the dataset
 tokenized_datasets = dataset.map(tokenize_function, batched=True)
@@ -28,31 +26,31 @@ split_datasets = tokenized_datasets.train_test_split(test_size=0.1)
 train_dataset = split_datasets['train']
 eval_dataset = split_datasets['test']
 
-#Model/Trainer setup
+# Model/Trainer setup
 training_args = TrainingArguments(
     output_dir='./results',
-    eval_strategy="epoch",
-    num_train_epochs=10,
+    num_train_epochs=5,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
     weight_decay=0.01,
     logging_dir='./logs',
-    learning_rate=5e-5
+    learning_rate=2e-04,
+    lr_scheduler_type='linear',  
+    eval_strategy="epoch"
 )
 
-model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=3)
-
-
-#Creating a trainer
+# Create a trainer
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=train_dataset,  
-    eval_dataset=eval_dataset,    
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
     tokenizer=tokenizer
 )
 
+# Train the model
 trainer.train()
 
+# Save the model and tokenizer
 model.save_pretrained('./saved_model')
 tokenizer.save_pretrained('./saved_model')
