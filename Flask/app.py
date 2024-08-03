f4fba044186ff5712a2e49f784b57fbbc2a5fd54
Hudson Kass <kass.h@northeastern.edu>
2024-08-04 02:21:01 +0300

    added subscribe api

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -7,17 +7,34 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///subscribers.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db, 'migrations')
+migrate = Migrate(app, db)
 
 class Subscriber(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True, nullable=False)
-    
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
+
+@app.route('/subscribe', methods=['POST'])
+def add_subscriber():
+    email = request.form.get('email')
+    if not email:
+        return "No email provided, please enter an email.", 400
+    
+    existing_subscriber = Subscriber.query.filter_by(email=email).first()
+    if existing_subscriber is not None:
+        return "Email already subscribed. Please use another email.", 409
+    
+    try:
+        new_subscriber = Subscriber(email=email)
+        db.session.add(new_subscriber)
+        db.session.commit()
+        return "Subscription successful!", 200
+    except Exception as e:
+        print(str(e))
+        return "Error processing your subscription.", 500
 
 if __name__ == "__main__":
     app.run(debug=True)
