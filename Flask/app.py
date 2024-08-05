98b0316480b463ea39875dde3d4b9d82bf2f2b3b
Hudson Kass <kass.h@northeastern.edu>
2024-08-06 01:16:22 +0300

    I HATE VERCEL

--- Flask/app.py
+++ Flask/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
-from verify_email import verify_email  # Ensure this matches the file name exactly
+from Flask.verify_email import verify_email  # Ensure this matches the file name exactly
 
 app = Flask(__name__)
 load_dotenv()
