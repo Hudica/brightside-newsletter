d08c571508730a017237107e7a8d3df1e988f9c9
Hudson Kass <kass.h@northeastern.edu>
2024-08-05 23:12:59 +0300

    connected to a mongo db

--- Flask/app.py
+++ Flask/app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 from verify_email import verify_email
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///subscribers.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+load_dotenv()
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-class Subscriber(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), unique=True, nullable=False)
+# Set up MongoDB connection
+mongo_conn_string = os.getenv('MONGO_CONN_STRING')
+client = MongoClient(mongo_conn_string)
+db = client.BrightSideUsers
+subscribers = db.emails
 
 @app.route('/')
 def index():
@@ -28,39 +27,30 @@ def add_subscriber():
     if not email:
         return "No email provided, please enter an email.", 400
     
-    existing_subscriber = Subscriber.query.filter_by(email=email).first()
-    if existing_subscriber is not None:
+    if subscribers.find_one({"email": email}):
         return "Email already subscribed. Please use another email.", 409
     
-    verification_result = verify_email(email)
+    verification_result = verify_email(email)  # Ensure you handle email verification appropriately
     if verification_result and verification_result['data']['status'] == 'valid':
         try:
-            new_subscriber = Subscriber(email=email)
-            db.session.add(new_subscriber)
-            db.session.commit()
+            subscribers.insert_one({"email": email})
             return "Subscription successful!", 200
         except Exception as e:
             print(str(e))
-        return "Error processing your subscription.", 500
+            return "Error processing your subscription.", 500
     else:
         return "Please provide a valid email address.", 400
+
 @app.route('/unsubscribeAction', methods=['POST'])
 def unsubscribeAction():
     email = request.form.get('email')
     if not email:
         return "No email provided, please enter an email.", 400
 
-    subscriber = Subscriber.query.filter_by(email=email).first()
-    if subscriber:
-        try:
-            db.session.delete(subscriber)
-            db.session.commit()
-            return "You have been successfully unsubscribed.", 200
-        except Exception as e:
-            print(str(e))
-            return "Error processing your unsubscribe request.", 500
+    if subscribers.delete_one({"email": email}).deleted_count > 0:
+        return "You have been successfully unsubscribed.", 200
     else:
         return "Email not found in our database.", 404
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
