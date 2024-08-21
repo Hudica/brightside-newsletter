617de180ba89ce4b739b6444345fe4dd5ab33ea9
Hudson Kass <kass.h@northeastern.edu>
2024-08-21 15:38:45 -0700

    email styling

--- Flask/send_email_test.py
+++ Flask/send_email_test.py
@@ -26,33 +26,51 @@ subscribers_collection = db.emails
 def fetch_recipients():
     emails = [{'Email': doc['email']} for doc in subscribers_collection.find({}, {"_id": 0, "email": 1})]
     return emails
-def generate_html_body(headlines, urls, description):
-    html_body = """
+def generate_html_body(headlines, urls, description, domains):
+    background_url = "https://images.unsplash.com/photo-1548614229-c1fe21dfab63?q=80&w=2565&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90oy1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    fallback_color = "#242124"  # Dark grey
+
+    html_body = f"""
     <html>
-    <body style="font-family: 'Helvetica', 'Arial', sans-serif; margin: 0; padding: 20px; color: #f0f0f0; background-color: #1e1e1e; background: linear-gradient(to right, #3a3a3a, #1e1e1e); text-align: center;">
-        <h1 style="color: #ffd700; margin-bottom: 20px; font-size: 2.5em;">Here is The Brightside Newsletter!</h1>
-        <p style="font-size: 1.2em; color: #dcdcdc; margin-bottom: 20px;">We provide the latest updates from positive and interesting news worldwide, delivered straight to your inbox. These articles were classified to be positive by our custom AI model.</p>
-        <table style="width: 100%; max-width: 600px; margin: 0 auto;">
+    <body style="font-family: 'Helvetica', 'Arial', sans-serif; margin: 0; padding: 20px; text-align: center; background-color: {fallback_color}; background-image: url('{background_url}'); background-size: cover; background-position: center;">
+        <!--[if gte mso 9]>
+        <v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+            <v:fill type="tile" src="{background_url}" color="{fallback_color}"/>
+        </v:background>
+        <![endif]-->
+        <h1 style="color: #fcc200; margin-bottom: 20px; font-size: 2.75em;">The Brightside Newsletter!</h1>
+        <p style="color: #a9a9a9; font-size: 1.4em; margin-bottom: 20px;">We provide the latest updates from positive and interesting news worldwide, delivered straight to your inbox. These articles were classified to be positive by our custom AI model.</p>
+        <table style="width: 100%; max-width: 600px; margin: 0 auto; background-color: rgba(0, 0, 0, 0.8); box-shadow: 0px 0px 10px rgba(0,0,0,0.3);">
     """
     for headline, url, desc, domain in zip(headlines, urls, description, domains):
         html_body += f"""
             <tr>
-                <td style="background-color: #2e2e2e; border: 2px solid #444; padding: 20px; border-radius: 12px; margin-bottom: 20px;">
-                    <a href="{url}" target="_blank" style="color: #ffd700; text-decoration: none; font-size: 1.5em;">{headline}</a>
-                    <p style="font-size: 1em; color: #fff5ee; margin-top: 10px;">{desc}</p>
-                    <p style = "font-size: 1em; color: #dcdcdc; margin-top 5px;">(Credit: {domain})</p>
+                <td style="background-color: #fffafa; border: 1px solid #dcdcdc; padding: 20px; border-radius: 12px; margin-bottom: 20px;">
+                    <a href="{url}" target="_blank" style="color: #3366cc; text-decoration: none; font-size: 1.5em;">{headline}</a>
+                    <p style="font-size: 1em; color: #444444; margin-top: 10px;">{desc}</p>
+                    <p style="font-size: 1em; color: #666666; margin-top: 5px;">(Credit: {domain})</p>
                 </td>
             </tr>
         """
-    html_body += """
+    html_body += f"""
         </table>
-        <p style="font-size: 1.2em; color: #dcdcdc;">Thank you for subscribing to our newsletter. Stay tuned for more updates!</p>
-        <p style="font-size: 1.2em; color: #dcdcdc;">To contact the newsletter with any questions or concerns, please reach out at <a href="mailto:brightside-news@hudica.info" style="color: #ffd700;">this email</a>.</p>
-        <p style="font-size: 1.2em; color: #dcdcdc;">To unsubscribe from our newsletter, please click <a href="https://news.hudica.info/unsubscribe" style="color: #ffd700; text-decoration: none;"><u>here.</u></a></p>
+        <p style="color: #696969; font-size: 1.2em;">Thank you for subscribing to our newsletter. Stay tuned for more updates!</p>
+        <p style="color: #696969; font-size: 1.2em;">To contact the newsletter with any questions or concerns, please reach out <a href="mailto:brightside-news@hudica.info" style="color: #3366cc;">here</a>.</p>
+        <div style="color: #696969; text-align: center; padding: 20px; font-size: 0.9em; border-top: 1px solid #444;">
+            <p>To unsubscribe from our newsletter, please <a href="https://news.hudica.info/unsubscribe" style="color: #3366cc; text-decoration: none;">click here</a>.</p>
+        </div>
     </body>
     </html>
     """
     return html_body
+
+
+
+
+
+
+
+
 
 
 
@@ -78,5 +96,5 @@ singleRecipient = [{'Email': 'hudson@kass.net'}]
 
 # Main execution
 recipients = fetch_recipients()
-html_body = generate_html_body(headlines, urls, description)
+html_body = generate_html_body(headlines, urls, description, domains)
 send_email(html_body, singleRecipient)
